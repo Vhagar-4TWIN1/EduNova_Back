109d64bd2b0c9626cb0996aa550eead43466f157
assilbelhaj <100833883+assilbelhaj@users.noreply.github.com>
2025-02-18 18:56:51 +0100

    login/logout/changepass/jwt/hashcode/emailVerification

---
+++ index.js
@@ -0,0 +1,35 @@
+require('dotenv').config();
+console.log("MongoDB URI:", process.env.MONGODB_URI); // Debugging
+console.log("Port:", process.env.PORT);
+
+const express = require('express');
+const helmet = require('helmet');
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
+const mongoose = require('mongoose');
+const authRouter = require('./routers/authRouter');
+const app = express();
+
+app.use(cors());
+app.use(helmet());
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+mongoose
+	.connect(process.env.MONGODB_URI)
+	.then(() => {
+		console.log('Connected to MongoDB');
+	})
+	.catch((error) => {
+		console.error('MongoDB connection error:', error);
+	});
+
+app.use('/api/auth', authRouter);
+app.get('/', (req, res) => {
+	res.json({ message: 'Hello from the server' });
+});
+
+app.listen(process.env.PORT || 3000, () => {
+	console.log(`Listening on port ${process.env.PORT || 3000}...`); // FIXED string interpolation
+});
