4b91bfcdddb9a13eb5ad5bc357683bb065edde10
ChihebDabebi <dabebi.chiheb@esprit.tn>
2025-04-10 02:23:36 +0100

    fix

--- routers/moduleRouter.js
+++ routers/moduleRouter.js
@@ -1,14 +1,7 @@
-<<<<<<< HEAD
 const express = require("express");
-=======
-const express = require('express');
-const { createModule, getModules, updateModule, deleteModule,getModuleWithId } = require('../controllers/moduleController');
-
->>>>>>> fed8773005fb90fef6ed93423fe5792c4dcacda4
 const router = express.Router();
 const lessonController = require("../controllers/lessonController"); // ✅ Ajoutez cette ligne
 
-<<<<<<< HEAD
 const {
   createModule,
   getModules,
@@ -23,12 +16,5 @@ router.get("/", getModules);
 router.put("/:id", updateModule);
 router.delete("/:id", deleteModule);
 router.get("/modules/:moduleId/lessons", lessonController.getLessonsByModule);
-=======
-router.post('/add', createModule);
-router.get('/', getModules);
-router.get('/:id',getModuleWithId);
-router.put('/:id', updateModule);
-router.delete('/:id', deleteModule);
->>>>>>> fed8773005fb90fef6ed93423fe5792c4dcacda4
 
 module.exports = router;
