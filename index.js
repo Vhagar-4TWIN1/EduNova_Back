b3eade55b68ef425fe50d37d438b568057093553
ChihebDabebi <dabebi.chiheb@esprit.tn>
2025-05-07 09:36:13 +0100

    voice + toxicity detection

--- index.js
+++ index.js
@@ -66,8 +66,8 @@ app.use(
   })
 );
 app.use(cookieParser());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
 // Serve static files from uploads directory
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
